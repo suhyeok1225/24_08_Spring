0d15e00735aa56dda81651695d0f8a0aa8c6b87e
suhyeok1225 <chaesh1225@gmail.com>
2024-08-09 13:18:02 +0900

    게시물 테스트 데이터 생성

--- demo/src/main/java/com/example/demo/controller/UsrArticleController.java
+++ demo/src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -16,14 +16,29 @@ public class UsrArticleController {
 	public UsrArticleController(){
 		lastArticleId = 0;
 		articles = new ArrayList<>();
+		makeTestData();
 	}
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+	private void makeTestData() {
+		for(int i = 0; i <= 10; i ++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+			writeArticle(title, body);
+			
+		}
+		
+	}
+	public Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
 		Article article = new Article(id, title, body);
 		articles.add(article);
 		lastArticleId++;
+		return article;
+	}
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
+		articles.add(article);
 		return article;
 	}
 	@RequestMapping("/usr/article/getArticles")
@@ -33,3 +48,4 @@ public class UsrArticleController {
 	}
 	
 }
+
--- demo/src/main/java/com/example/demo/controller/UsrHomeController.java
+++ demo/src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,80 +1,9 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Controller
 public class UsrHomeController {
-	private int count;
-	public UsrHomeController() {
-		count = 0;
-	}
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕";
-	}
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 3;
-	}
-	@RequestMapping("/usr/home/getString")
-	@ResponseBody
-	public String getString() {
-		return "hi";
-	}
-	@RequestMapping("/usr/home/getMap")
-	@ResponseBody
-	public Map<String, Object> getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수나이", 11);
-		map.put("영수나이", 12);
-
-		return map;
-	}
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	public Double getDouble() {
-		return 3.14;
-	}
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public Boolean getBoolean() {
-		return true;
-	}
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article(1, "제목", "내용");
-		return article;
-	}
-	@RequestMapping("/usr/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("철수 나이");
-		list.add("영수 나이");
-		return list;
-	}
-}
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Article {
-
-	int id;
-	String title;
-	String body;
+	
 }
 
